2c55dbe6122094d646a37f92357f34befb9f0465
rubayet-islam <season.ss2001@gmail.com>
2023-01-05 22:24:18 +0600

    navbar responsive designing adjusted

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -13,29 +13,17 @@ const Navbar = () => {
         <div className="dropdowns">
           <div className="dropdown">
             <button className="button">
-              Services
-              <img src={chevron} alt='' />
+              Home
             </button>
-            <div className="dropdown-menu">
-              <button>UX/UI Design</button>
-              <button>Web Applications</button>
-              <button>SEO Advice</button>
-            </div>
           </div>
           <div className="dropdown">
             <button className="button">
               Products
-              <img src={chevron} alt='' />
             </button>
-            <div className="dropdown-menu">
-              <button>Learn CSS Ebook</button>
-              <button>Security Course</button>
-              <button>Masterclass Bundle</button>
-            </div>
           </div>
           <div className="dropdown">
             <button className="button">
-              Support
+             Categories
               <img src={chevron} alt='' />
             </button>
             <div className="dropdown-menu">
@@ -43,6 +31,11 @@ const Navbar = () => {
               <button>Send Email</button>
               <button>Request Help</button>
             </div>
+          </div>
+          <div className="dropdown">
+            <button className="button">
+             Login
+            </button>
           </div>
         </div>
       </nav>
