cd47b407a7e5a13f48f2a985689986cfd4d4147b
rubayet-islam <season.ss2001@gmail.com>
2023-01-05 21:21:29 +0600

    logo and custom truetype font added

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import chevron from './images/chevron.svg';
 import './Navbar.css';
+import mengary from './images/logo.jpg';
 
 const Navbar = () => {
     const toggleMenu = () => document.body.classList.toggle("open");
     return (
-            <nav className="navbar">
+            <nav className="navbar px-4">
         <button onClick={toggleMenu} className="burger"></button>
-        <button className="button">Mengary</button>
+        <img src={mengary} className='logo' alt="" />
+        <button className="button mengary">Mengary</button>
         <div className="dropdowns">
           <div className="dropdown">
             <button className="button">
