accb87c0369b3b05599276c0362cc56b282c091b
rubayet-islam <season.ss2001@gmail.com>
2023-01-11 18:42:39 +0600

    navbar fixed

--- src/Pages/Main/Home/Home.js
+++ src/Pages/Main/Home/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
            <span className="underline"><span className=" font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-600 via-rose-800 to-gray-800 animate-text"> Mengary</span></span> and wear
             fashionable.
           </h1>
-          <p className="px-4 mt-8 mb-12 text-lg">
+          <p className="px-4 mt-8 mb-6 text-lg">
             Wore by more than 10 million shoppers worldwide.
           </p>
         </div>
--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -1,50 +1,36 @@
 import React from 'react';
+import { CiShoppingCart } from "react-icons/ci";
+import { Link } from 'react-router-dom';
+import './Header.css';
+
 
 const Header = () => {
     return (
-        <div className="navbar bg-black text-white">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-            </label>
-            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-              <li><a>Item 1</a></li>
-              <li tabIndex={0}>
-        <a>
-          Parent
-          <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-        </a>
-        <ul className="p-2 bg-base-100">
-          <li><a>Submenu 1</a></li>
-          <li><a>Submenu 2</a></li>
-        </ul>
-      </li>
-              <li><a>Item 3</a></li>
-            </ul>
-          </div>
-          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li><a>Item 1</a></li>
-            <li tabIndex={0}>
-              <a>
-                Parent
-                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-              </a>
-              <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
-            </li>
-            <li><a>Item 3</a></li>
+      <div className="navbar bg-[#151515] text-gray-400">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+          </label>
+          <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-black rounded-box w-52">
+            <li className='hover:text-white'><Link>Home</Link></li>
+            <li className='hover:text-white'><Link>All Products</Link></li>
+            <li className='hover:text-white'><Link>Categories</Link></li>
           </ul>
         </div>
-        <div className="navbar-end">
-          <a className="btn">Get started</a>
-        </div>
+        <Link className="btn btn-ghost normal-case text-xl mengary">Mengary</Link>
       </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+        <li className='hover:text-white'><Link>Home</Link></li>
+            <li className='hover:text-white'><Link>All Products</Link></li>
+            <li className='hover:text-white'><Link>Categories</Link></li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <Link className="btn z-[100] bg-transparent hover:bg-transparent rounded-none cart-btn"><CiShoppingCart className='text-xl' />&nbsp;Cart</Link>
+      </div>
+    </div>
     );
 };
 
