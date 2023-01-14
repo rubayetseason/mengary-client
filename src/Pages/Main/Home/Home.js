6a9314abe2c57d7e9d152feded6dd41d1ea98b8f
rubayet-islam <season.ss2001@gmail.com>
2023-01-14 19:04:46 +0600

    hero section changed

--- src/Pages/Main/Home/Home.js
+++ src/Pages/Main/Home/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Carousel from "../../../Components/Carousel/Carousel";
 import Deal from "../../../Components/Deal";
-import Hero from "../../../Components/Hero";
+import RealHero from "../../../Components/RealHero";
 import Review from "../../../Components/Review";
 import SecondHero from "../../../Components/SecondHero";
 
@@ -9,14 +9,19 @@ const Home = () => {
   return (
     <div>
       <section>
-        <Hero></Hero>
+        <RealHero></RealHero>
       </section>
       <section className="bg-[#151515] text-gray-100">
         <div className="container mx-auto flex flex-col items-center px-5 py-16 text-center md:py-32 md:px-10 lg:px-32 xl:max-w-3xl">
           <h1 className="text-4xl font-bold leading-none sm:text-5xl">
             Enjoy your shopping experience with
-           <span className="underline"><span className=" font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-600 via-rose-800 to-gray-800 animate-text"> Mengary</span></span> and wear
-            fashionable.
+            <span className="underline">
+              <span className=" font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-600 via-rose-800 to-gray-800 animate-text">
+                {" "}
+                Mengary
+              </span>
+            </span>{" "}
+            and wear fashionable.
           </h1>
           <p className="px-4 mt-8 mb-6 text-lg">
             Wore by more than 10 million shoppers worldwide.
