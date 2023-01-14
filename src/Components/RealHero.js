d777664d492e3c966dc69cfbd5e3f65ad2c3c074
rubayet-islam <season.ss2001@gmail.com>
2023-01-14 19:17:45 +0600

    responsiveness added

--- src/Components/RealHero.js
+++ src/Components/RealHero.js
@@ -2,17 +2,18 @@ import React from 'react';
 import hero1 from './images/hero-1.jpg';
 import hero2 from './images/hero-2.jpg';
 import { BsArrowRight } from "react-icons/bs";
+import './Hero.css';
 
 const RealHero = () => {
     return (
         <div className="carousel bg-[#F3F2EE] w-full">
   <div id="slide1" className="carousel-item relative w-full">
-    <div className='grid grid-cols-2'>
-        <div className='my-auto pl-14'>
-            <div className='text-red-600 tracking-widest text-sm mb-8'>SUMMER COLLECTION - 2023</div>
-            <div className='text-5xl font-semibold mb-4'>Welcome to <span className=" font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-600 via-rose-800 to-gray-800 animate-text"> Mengary.</span></div>
-            <div className='text-lg font-light mb-4'>Exciting offers. Fresh style. Luxury essentials. Max comfort.</div>
-            <div> <button className="btn tracking-widest px-6 bg-transparent hover:bg-transparent shop-btn rounded-none z-[100]">
+    <div className='grid grid-cols-1 md:grid-cols-2'>
+        <div className='my-auto pl-5 md:pl-14'>
+            <div className='text-red-600 tracking-widest text-sm mb-8 mt-8 md:mt-0'>SUMMER COLLECTION - 2023</div>
+            <div className='text-4xl md:text-5xl font-semibold mb-4'>Welcome to the world of <span className=" font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-600 via-rose-800 to-gray-800 animate-text"> Mengary.</span></div>
+            <div className='text-md md:text-lg font-light mb-5'>Exciting offers. Fresh style. Luxury essentials. Max comfort.</div>
+            <div> <button className="btn tracking-widest px-3 bg-transparent hover:bg-transparent shop-btn rounded-none z-[100]">
               Shop Now &nbsp;
               <BsArrowRight />
             </button></div>
@@ -25,12 +26,12 @@ const RealHero = () => {
     </div>
   </div> 
   <div id="slide2" className="carousel-item relative w-full">
-  <div className='grid grid-cols-2'>
-  <div className='my-auto pl-14'>
-            <div className='text-red-600 tracking-widest text-sm mb-8'>SUMMER COLLECTION - 2023</div>
-            <div className='text-5xl font-semibold mb-4'>Welcome to <span className=" font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-600 via-rose-800 to-gray-800 animate-text"> Mengary.</span></div>
-            <div className='text-lg font-light mb-4'>Exciting offers. Fresh style. Luxury essentials. Max comfort.</div>
-            <div> <button className="btn tracking-widest px-6 bg-transparent hover:bg-transparent shop-btn rounded-none z-[100]">
+  <div className='grid grid-cols-1 md:grid-cols-2'>
+  <div className='my-auto pl-5 md:pl-14'>
+            <div className='text-red-600 tracking-widest text-sm mb-8 mt-8 md:mt-0'>SUMMER COLLECTION - 2023</div>
+            <div className='text-4xl md:text-5xl font-semibold mb-4'>Welcome to the world of <span className=" font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-600 via-rose-800 to-gray-800 animate-text"> Mengary.</span></div>
+            <div className='text-md md:text-lg font-light mb-5'>Exciting offers. Fresh style. Luxury essentials. Max comfort.</div>
+            <div> <button className="btn tracking-widest px-3 bg-transparent hover:bg-transparent shop-btn rounded-none z-[100]">
               Shop Now &nbsp;
               <BsArrowRight />
             </button></div>
