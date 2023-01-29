b95df622a91e4cb4f2127dbc67c8039e33130a2e
rubayet-islam <season.ss2001@gmail.com>
2023-01-29 22:05:46 +0600

    minor changes

--- src/Pages/Main/AllProducts/Products.js
+++ src/Pages/Main/AllProducts/Products.js
@@ -7,7 +7,7 @@ import { CiSearch } from "react-icons/ci";
 import { useState } from "react";
 
 const Products = () => {
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
 
   const handleSearch = (event) => {
     event.preventDefault();
@@ -61,9 +61,11 @@ const Products = () => {
             </div>
           </div>
         </form>
-        <p className="mt-4 text-gray-500">Found {products.length} available items</p>
+        <p className="mt-4 text-gray-500">
+          Found {products.length} available items
+        </p>
       </section>
-      <div className="px-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+      <div className="px-5 mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         {products.map((product) => (
           <Product product={product} key={product._id}></Product>
         ))}
